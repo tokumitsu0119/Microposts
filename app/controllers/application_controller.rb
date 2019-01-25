9abd248ec00e599fc0d1e37cd3e5ccac33e0f9b4
tokumitsu <mitsuhikotokuhisa.apu@gmail.com>
2019-01-25 08:04:19 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,15 @@ class ApplicationController < ActionController::Base
     end
   end
 end
+
+  private
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+  end
